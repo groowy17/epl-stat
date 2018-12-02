37fe1a9d40f53ff63630bc3e86afa9ba52487c29
Коноплев Павел <konoplev@reg.ru>
2018-12-03 00:03:23 +0300

    Styles for matches

--- src/components/Match.js
+++ src/components/Match.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { teamName } from "../utils";
+import "../styles/Match.css";
 class Match extends Component {
   render() {
     const match = this.props.match;
@@ -7,16 +8,32 @@ class Match extends Component {
     const crestAwayUrl = this.props.crestAway.crestUrl;
 
     return (
-      <div className="row">
-        <span className="five columns">
-          {teamName(match.homeTeam.name)}
-        </span>
-        <span className="two columns">
-          {match.score.fullTime.homeTeam} - {match.score.fullTime.awayTeam}
-        </span>
-        <span className="five columns">
-          {teamName(match.awayTeam.name)}
-        </span>
+      <div className="row match">
+        <div className="five columns match-team">
+          <div className="row">
+            <div className="ten columns match-team__name match-team__name_home">
+              {teamName(match.homeTeam.name)}
+            </div>
+            <div className="two columns match-team__crest">
+              <img src={crestHomeUrl} alt={match.homeTeam.name} />
+            </div>
+          </div>
+        </div>
+        <div className="two columns match-score">
+          <span className="match-score__result">
+            {match.score.fullTime.homeTeam} - {match.score.fullTime.awayTeam}
+          </span>
+        </div>
+        <div className="five columns match-team">
+          <div className="row">
+            <div className="two columns match-team__crest">
+              <img src={crestAwayUrl} alt={match.awayTeam.name} />
+            </div>
+            <div className="ten columns match-team__name match-team__name_away">
+              {teamName(match.awayTeam.name)}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
